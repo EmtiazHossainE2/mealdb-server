46a9d36638db8003ae40e123b961fe1f936f89d2
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-24 00:53:22 +0600

    curd operation done

--- index.js
+++ index.js
@@ -27,37 +27,53 @@ async function run() {
         const foodCollection = client.db("mealdb").collection("food");
 
         //8CRUD ==> R [get means load data] findMultiple
-        app.get('/food' , async(req,res) => {
-            const query = {} 
-            const cursor =  foodCollection.find(query)
+        app.get('/food', async (req, res) => {
+            const query = {}
+            const cursor = foodCollection.find(query)
             const foods = await cursor.toArray()
             res.send(foods)
         })
         //9 find one 
-        app.get('/food/:id' , async(req,res) => {
-            const id = req.params.id 
-            const query = {_id : ObjectId(id)}
+        app.get('/food/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
             const food = await foodCollection.findOne(query)
             res.send(food)
         })
 
         //10 post insertOne
-        app.post('/food' , async(req,res) => {
-            const food = req.body 
+        app.post('/food', async (req, res) => {
+            const food = req.body
             const result = await foodCollection.insertOne(food)
             res.send(result)
         })
 
         //11 delete 
-        app.get('/food/:id' , async(req,res) => {
-            const id = req.params.id 
-            const query = {_id: ObjectId(id)}
+        app.get('/food/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
             const result = await foodCollection.deleteOne(query)
-            
-
+            res.send(result)
         })
 
         //12 update 
+        app.put('/food/:id', async (req, res) => {
+            const id = req.params.id
+            const updateUser = req.body
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            // create a document that sets the plot of the food
+            const updateDoc = {
+                $set: {
+                    name : updateUser.name , 
+                    title : updateUser.title,
+                    img : updateUser.img ,
+                    price : updateUser.price
+                },
+            };
+            const result = await foodCollection.updateOne(filter, updateDoc, options);
+            console.log(result);
+        })
 
 
     } finally {
