3b9875b4a75c7001fdb780f83c5e74eb1ab4e6c5
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-24 00:34:33 +0600

    get and post data

---
+++ index.js
@@ -0,0 +1,77 @@
+//1basic
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+//3
+const app = express()
+const port = process.env.PORT || 5000
+
+require('dotenv').config()
+
+//4middleware
+app.use(cors())
+app.use(express.json())
+
+
+//5 mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntqc6.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//6
+// https://www.mongodb.com/docs/drivers/node/current/usage-examples/find/ 
+async function run() {
+    try {
+        //7
+        await client.connect();
+        const foodCollection = client.db("mealdb").collection("food");
+
+        //8CRUD ==> R [get means load data] findMultiple
+        app.get('/food' , async(req,res) => {
+            const query = {} 
+            const cursor =  foodCollection.find(query)
+            const foods = await cursor.toArray()
+            res.send(foods)
+        })
+        //9 find one 
+        app.get('/food/:id' , async(req,res) => {
+            const id = req.params.id 
+            const query = {_id : ObjectId(id)}
+            const food = await foodCollection.findOne(query)
+            res.send(food)
+        })
+
+        //10 post insertOne
+        app.post('/food' , async(req,res) => {
+            const food = req.body 
+            const result = await foodCollection.insertOne(food)
+            res.send(result)
+        })
+
+        //11 delete 
+        app.get('/food/:id' , async(req,res) => {
+            const id = req.params.id 
+            const query = {_id: ObjectId(id)}
+            const result = await foodCollection.deleteOne(query)
+            
+
+        })
+
+        //12 update 
+
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+//2test
+
+app.get('/', (req, res) => {
+    res.send('Mealdb Server Working')
+})
+
+app.listen(port, () => {
+    console.log("Running Mealdb on", port);
+})
